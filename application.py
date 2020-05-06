d8d92eefb39b30db4cd609cda969d65ad4c4c669
Abhinavbajimaya <abhinavbajimya1@gmail.com>
2020-05-06 22:43:13 +0545

    3.Can Register

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
+#do the imports here
 import os
-
-from flask import Flask, session,render_template
+from flask import Flask, session,render_template,request,flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -8,9 +8,10 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField
 from wtforms.validators import InputRequired,email_validator,Length,Email
+import requests
 
 
-
+#setup
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SECRET_KEY']="abhinav"
@@ -28,19 +29,21 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+
+#Login form
 class LoginForm(FlaskForm):
     username=StringField('username',validators=[InputRequired(),Length(min=4,max=18)])
     password=PasswordField('password',validators=[InputRequired(),Length(min=8,max=18)])
     remember=BooleanField('Remember me')
 
+#Registration form
 class RegisterForm(FlaskForm):
     username=StringField('username',validators=[InputRequired(),Length(min=4,max=18)])
     email=StringField('email',validators=[InputRequired(), Email(), Length(max=50)])
     password=PasswordField('password',validators=[InputRequired(),Length(min=8,max=18)])
 
 
-
-
+#Routes
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -49,9 +52,17 @@ def index():
 def register():
     form=RegisterForm()
     if form.validate_on_submit():
-        return('<h1>'+form.username.data+ '</h1>')
-
+        username=request.form.get("username")
+        email=request.form.get("email")
+        password=request.form.get("password")
+        db.execute("INSERT INTO users (username,email,password) VALUES (:username,:email,:password)",
+        {"username":username,"email":email,"password":password} )
+        db.commit()
+        flash('Registered succesfully')   
+       # return render_template("index.html",message="Registration Successful")
+    
     return render_template("register.html", form=form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
